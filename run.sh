a256d466a021ac10f73337b9bde4e661c899b6b1
Bryne Lee <bryne_lxd@sina.com>
2020-02-13 21:47:19 +0800

    added Docker support for mysqldb server access.

--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@ echo "********************************************************"
 echo "Starting Transaction Agent Service ..."
 echo "********************************************************"
 java -Ddebug=$DEBUG_MODE \
-     -Dserver.port=$SERVERPORT -jar /usr/src/app/transactionagentservice-0.0.1-SNAPSHOT.jar
+     -Dserver.port=$SERVERPORT \
+     -Dspring.datasource.url=$SPRINGDATASOURCEURL \
+     -Dspring.datasource.username=$DBUSERNAME \
+     -Dspring.datasource.password=$DBPASSWORD -jar /usr/src/app/transactionagentservice-0.0.1-SNAPSHOT.jar
--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -4,4 +4,7 @@ echo "********************************************************"
 echo "Starting Transaction Agent Service ..."
 echo "********************************************************"
 java -Ddebug=$DEBUG_MODE \
-     -Dserver.port=$SERVERPORT -jar /usr/local/usercenter/@project.build.finalName@.jar
+     -Dserver.port=$SERVERPORT \
+     -Dspring.datasource.url=$SPRINGDATASOURCEURL \
+     -Dspring.datasource.username=$DBUSERNAME \
+     -Dspring.datasource.password=$DBPASSWORD -jar /usr/local/usercenter/@project.build.finalName@.jar
